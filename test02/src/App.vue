0698cc75d6aae9601bb71c0a6e32d69e9fde7d7a
wly233 <1316032679@qq.com>
2024-10-18 15:50:21 +0800

    222

---
+++ test02/src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <div id="app">
+    <tab-bar> </tab-bar>
+  </div>
+</template>
+
+<script>
+import TabBar from "./components/tabbar/TabBar.vue";
+
+export default {
+  name: "App",
+  components: {
+    TabBar,
+  },
+};
+</script>
